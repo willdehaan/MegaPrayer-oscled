708dbff8d5a7f6a93de6843802f4b2935a7bdd29
Kelly <kellyeogilvie@gmail.com>
2017-04-23 16:36:36 -0700

    initial commit to KO_makes_a_python_sim

--- env.sh
+++
@@ -1,7 +0,0 @@
-export VERSION=-4.9
-export HOST=arm-linux-gnueabihf
-export CC=${HOST}-gcc${VERSION}
-export CXX=${HOST}-g++${VERSION}
-export CFLAGS="-L$(pwd)/lib/${HOST}/lib -I$(pwd)/lib/arm-linux-gnueabihf/include"
-export CXXFLAGS="-L$(pwd)/lib/${HOST}/lib -I$(pwd)/lib/arm-linux-gnueabihf/include"
-export PREFIX=$(pwd)/lib/${HOST}
